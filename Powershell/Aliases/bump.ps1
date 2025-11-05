10152ab61db40de32ebbcb28cbcdaea6e1958138
bulga138 <57503100+bulga138@users.noreply.github.com>
2025-11-05 22:01:18 +0100

    feat(powershell): add aliases for bump commands
    This commit introduces a new set of PowerShell aliases to provide convenient shortcuts for the version bumping functions. These aliases simplify the process of incrementing version numbers, managing configuration, and auditing changes.
    
    The following aliases have been added:
    - `bump-major`: Alias for `Invoke-BumpMajor`
    - `bump-minor`: Alias for `Invoke-BumpMinor`
    - `bump-patch`: Alias for `Invoke-BumpPatch`
    - `bump-config`: Alias for `Set-BumpConfig`
    - `bump-audit`: Alias for `Show-BumpAudit`

---
+++ Powershell/Aliases/bump.ps1
@@ -0,0 +1,5 @@
+Set-Alias bump-major Invoke-BumpMajor
+Set-Alias bump-minor Invoke-BumpMinor
+Set-Alias bump-patch Invoke-BumpPatch
+Set-Alias bump-config Set-BumpConfig
+Set-Alias bump-audit Show-BumpAudit
